b0ec2e4772bdcb346de229646276a8466d117a72
b04901142 <31992609+b04901142@users.noreply.github.com>
2017-10-09 17:58:58 +0800

    Update peripheral.js

--- examples/pizza/peripheral.js
+++ examples/pizza/peripheral.js
@@ -1,5 +1,7 @@
 var util = require('util');
 
+var gpio = require('rpi-gpio');
+
 //
 // Require bleno peripheral library.
 // https://github.com/sandeepmistry/bleno
@@ -24,6 +26,12 @@ var PizzaService = require('./pizza-service');
 //
 var name = 'PizzaSquat';
 var pizzaService = new PizzaService(new pizza.Pizza());
+
+gpio.on('change', function(channel, value) {
+  console.log('Channel ' + channel + ' value is now ' + value);
+  pizza.button = value;
+});
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 //
 // Wait until the BLE radio powers on before attempting to advertise.
