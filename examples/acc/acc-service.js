01c4172aaf22be5e23fff955c6b5c2bb4447ff82
b04901142 <b04901142@ntu.edu.tw>
2017-10-25 19:32:46 +0800

    I love TingTing

---
+++ examples/acc/acc-service.js
@@ -0,0 +1,21 @@
+var util = require('util');
+var bleno = require('../..');
+
+var AccXCharacteristic = require('./acc-x-characterisic');
+var AccYCharacteristic = require('./acc-y-characterisic');
+var AccZCharacteristic = require('./acc-z-characterisic');
+
+function AccService() {
+	bleno.PrimaryService.call(this, {
+		uuid: '13333333333333333333333333333337',
+		characteristics: [
+			new AccXCharacteristic(),
+			new AccYCharacteristic(),
+			new AccZCharacteristic()
+		]
+	});
+}
+
+util.inherits(AccService, bleno.PrimaryService);
+
+module.exports = AccService;
---
+++ examples/acc/acc-x-characterisic.js
@@ -0,0 +1,85 @@
+var util = require('util');
+var bleno = require('../..');
+
+const ADXL345 = require('adxl345-sensor');
+
+const options = {
+  i2cBusNo   : 1, // defaults to 1
+  i2cAddress : ADXL345.I2C_ADDRESS_ALT_GROUNDED() // defaults to 0x53
+};
+
+const adxl345 = new ADXL345(options);
+
+function AccXCharacteristic(){
+	bleno.Characteristic.call(this, {
+		uuid: '13333333333333333333333333330001',
+		properties: ['read'],
+		descriptors: [
+			new bleno.Descriptor({
+				uuid: '2901',
+				value: 'Gets acc x.'
+			})
+		]
+	});
+	this.pizza = pizza;	
+}
+
+util.inherits(AccXCharacteristic, bleno.Characteristic);
+
+//AccXCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
+//	if (offset) {
+//		callback(this.RESULT_ATTR_NOT_LONG);
+//	}
+//	else if (data.length !== 2) {
+//		callback(this.RESULT_INVALID_ATTRIBUTE_LENGTH);
+//	}
+//	else {
+//		this.pizza.toppings = data.readUInt16BE(0);
+//		callback(this.RESULT_SUCCESS);
+//	}
+//};
+
+AccXCharacteristic.prototype.onReadRequest = function(offset, callback) {
+	if (offset) {
+		callback(this.RESULT_ATTR_NOT_LONG, null);
+	}
+	else {
+		var acc_x = new.Buffer(2)
+		adxl345.getAcceleration(true) // true for g-force units, else false for m/s²
+			.then((acceleration) => {
+				acc_x = acceleration["x"];
+			})
+			.catch((err) => {
+				console.log(`ADXL345 read error: ${err}`);
+				//setTimeout(getAcceleration, 2000);
+			});
+		callback(this.RESULT_SUCCESS, data.writeUInt16BE(3, 0)
+		//var data = new Buffer(2);
+		//data.writeUInt16BE(this.pizza.toppings, 0);
+		//callback(this.RESULT_SUCCESS, data);
+	}
+};
+
+module.exports = AccXCharacteristic;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
